440ea0b0120e7006425eba2c8887be88b170090e
muhtegaralfikri <105841110722@student.unismuh.ac.id>
2026-01-19 00:06:00 +0800

    feat: Add daily and monthly reports for admin, including filtering, statistics, data tables, Excel export, and a new Tailwind pagination view.

--- app/Views/pagers/tailwind_full.php
+++ app/Views/pagers/tailwind_full.php
@@ -36,7 +36,7 @@
                 <?php endif ?>
 
                 <?php foreach ($pager->links() as $link) : ?>
-                    <a href="<?= $link['uri'] ?>" class="relative inline-flex items-center px-4 py-2 text-sm font-semibold <?= $link['active'] ? 'z-10 bg-primary-600 text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600' : 'text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0' ?>">
+                    <a href="<?= $link['uri'] ?>" class="relative inline-flex items-center px-4 py-2 text-sm font-semibold <?= $link['active'] ? 'z-10 bg-blue-600 text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600' : 'text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0' ?>">
                         <?= $link['title'] ?>
                     </a>
                 <?php endforeach ?>
